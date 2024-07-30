751c45355d173ceef6f1713b6bb585e12467e64b
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-07-30 15:15:27 -0600

    Finished modal
    Finished modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden'); // Remove "hidden" class on the modal
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (
+  let i = 0;
+  i < btnsOpenModal.length;
+  i++ // Iterates through all the buttons
+)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
