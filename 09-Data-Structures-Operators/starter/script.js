679fafab6de206f6fc04fe868b24d04adaae12c5
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-08-07 09:39:44 -0600

    Finished Section 9
    Finished Section 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,27 @@
 'use strict';
 
-// Data needed for a later exercise
+/* ==================== String Methods Practice ==================== */
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50); // makes everything right aligned (everything is 50 characters long)
+  console.log(output);
+}
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -53,6 +72,7 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
 /* ==================== Coding Challenge: #4 ==================== */
 /* 
 Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
@@ -73,8 +93,6 @@ someVariable        ✅✅✅
 calculateAge        ✅✅✅✅
 delayedDeparture    ✅✅✅✅✅
 
-HINT 1: Remember which character defines a new line in the textarea 😉
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
 HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
 HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
 
@@ -83,23 +101,28 @@ Afterwards, test with your own test data!
 GOOD LUCK 😀
 */
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n'); // split by new line (enter)
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   // HINT 1: Remember which character defines a new line in the textarea 😉
+//   const rows = text.split('\n'); // Becomes an array with 5 elements
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
+//   // Loop over the array
+//   for (const [i, row] of rows.entries()) {
+//     // Need to get rid of spacing, lower the case, and then splitting the "_" so we get 2 words.
+//     const [first, second] = row.toLowerCase().trim().split('_');
 
-    const output = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase()
-    )}`;
-    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
-  }
-});
+//     // Taking the two words and replacing the first letter of the second word to upper case.
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     // Need to give every string the same length. Taking the index of each element so the checkmark repeats enough times.
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
 
 /* ==================== Working With Strings - Part 3 ==================== */
 // // Split and join
