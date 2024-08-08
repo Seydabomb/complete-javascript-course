dd8a0d724cc906b43061e3e3b80c391254b44963
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-08-08 13:58:31 -0600

    Started 146
    Started 146

--- Assignment Set 2/11-Arrays-Bankist/starter/script.js
+++ Assignment Set 2/11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,91 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+/////////////////////////////////////////////////
+/*======================= Simple Array Methods =========================*/
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// // SLICE -- does not impact original array
+// console.log(arr.slice(2)); // ['c', 'd', 'e']
+// console.log(arr.slice(2, 4)); // ['c', 'd']
+// console.log(arr.slice(-2)); // ['d','e']
+// console.log(arr.slice(-1)); // ['e']
+// console.log(arr.slice(1, -2)); // ['b','c']
+// console.log(arr.slice()); // ['a', 'b', 'c', 'd', 'e']
+// console.log([...arr]); // ['a', 'b', 'c', 'd', 'e']
+
+// // SPLICE -- mutates original array
+// // console.log(arr.splice(2)); // ['c', 'd', 'e']
+// arr.splice(-1);
+// console.log(arr); // ['a', 'b', 'c','d']
+// arr.splice(1, 2); // start at position 1, then remove 2 elements.
+// console.log(arr); // ['a','d']
+
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'j', 'f'];
+// console.log(arr2.reverse());
+
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters); // ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'g', 'h', 'i', 'j']
+// console.log([...arr, ...arr2]); // ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'g', 'h', 'i', 'j']
+
+// // JOIN
+// console.log(letters.join(' - ')); // a - b - c - d - e - f - j - g - h - i - j
+
+/*======================= At Method =========================*/
+// const arr = [23, 11, 64];
+// console.log(arr[0]); // 23
+// console.log(arr.at[0]); // 23
+
+// // getting last array element
+// console.log(arr[arr.length - 1]); // 64
+// console.log(arr.slice(-1)); // [64]
+// console.log(arr.slice(-1)[0]); // 64
+// console.log(arr.at(-1)); // 64
+
+// console.log('jonas'.at(0)); // j
+// console.log('jonas'.at(-1)); // s
+
+/*======================= Looping Arrays: forEach =========================*/
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// //for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// // forEach doesn't work if you need to break out of the loop. It always goes through the entirety of the array.
+// console.log(' ---- FOREACH ----');
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+// Our forEach function is doing:
+/* 
+0: function(200)
+1: function(450)
+2: function(400)
+etc.
+  */
+
+/*======================= forEach With Maps and Sets =========================*/
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
 
-/////////////////////////////////////////////////
+// Set
