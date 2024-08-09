e98a012940d1981a7b0dd8d3c55cbd83bc9975cf
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-08-09 09:52:29 -0600

    Done everything up to "The Filter Method"
    Done everything up to #153 "The Filter Method"

--- Assignment Set 2/11-Arrays-Bankist/starter/script.js
+++ Assignment Set 2/11-Arrays-Bankist/starter/script.js
@@ -63,6 +63,9 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /*======================= Creating DOM Elements =========================*/
 const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  // .textContent = 0;
+
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
@@ -80,10 +83,34 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+/*======================= Computing Usernames =========================*/
+
+// An example of an account to pull
+// const account1 = {
+//   owner: 'Jonas Schmedtmann',
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
+// const accounts = [account1, account2, account3, account4];
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts); // passing in accounts which becomes accs in the function.
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 /*======================= Simple Array Methods =========================*/
 // let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -177,3 +204,72 @@ etc.
 // currenciesUnique.forEach(function (value, _, map) {
 //   console.log(`${value}: ${value}`);
 // });
+
+/*======================= Coding Challenge #1 =========================*/
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   // #1
+//   const dogsJuliaCorrected = dogsJulia.slice(); // Slice provides a shallow copy of the original array
+//   dogsJuliaCorrected.splice(0, 1); // remove the first dog
+//   dogsJuliaCorrected.splice(-2); // remove the last two dogs
+//   // Alternatively, could have done dogsJulia.slice(1,3)
+//   // #2
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
+//   // #3
+//   dogs.forEach(function (dog, i) {
+//     if (dog >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+//     }
+//   });
+// };
+
+// // #4
+// // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+/*======================= Looping Arrays: forEach =========================*/
+// const eurToUsd = 1.1;
+// // const movementsUSD = movements.map(function (mov) {
+// //   return mov * eurToUsd;
+// // });
+
+// const movementsUSD = movements.map(mov => mov * eurToUsd); // take the mov array and make each element become mov*eurToUsd.
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// // Same result as above, just using a different methodology.
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+
+// const movementsDescriptions = movements.map(
+//   // map method is looping through all elements of the array and returning the string for each element.
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
+// console.log(movementsDescriptions);
+
+/*======================= The filter Method =========================*/
