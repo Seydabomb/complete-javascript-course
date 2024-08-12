001477e79de7996baa30a8ca7fd377374f6270fa
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-08-12 10:03:20 -0600

    Finished Section 11
    Finished Section 11

--- Assignment Set 2/11-Arrays-Bankist/starter/script.js
+++ Assignment Set 2/11-Arrays-Bankist/starter/script.js
@@ -653,27 +653,128 @@ GOOD LUCK 😀
 
 /*=================== Array Methods Practice =====================*/
 
-// #1
-const bankDepositSum = accounts
-  .flatMap(acc => acc.movements)
-  .filter(mov => mov > 0)
-  .reduce((sum, cur) => sum + cur, 0);
-console.log(bankDepositSum);
-
-// #2
-// Number of Deposits done with over $1000
-// const numDeposits1000 = account
+// // #1
+// const bankDepositSum = accounts
 //   .flatMap(acc => acc.movements)
-//   .filter(mov => mov >= 1000).length;
+//   .filter(mov => mov > 0)
+//   .reduce((sum, cur) => sum + cur, 0);
+// console.log(bankDepositSum);
 
-// reduce is starting the accumulator, count, at zero.
-const numDeposits1000 = accounts
-  .flatMap(acc => acc.movements)
-  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
-  .reduce((count, cur) => (cur >= 1000 ? count++ : count), 0);
+// // #2
+// // Number of Deposits done with over $1000
+// // const numDeposits1000 = account
+// //   .flatMap(acc => acc.movements)
+// //   .filter(mov => mov >= 1000).length;
 
-console.log(numDeposits1000);
+// // reduce is starting the accumulator, count, at zero.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0); // the arrow function implicitely returns the accumulator, count.
 
-let a = 10;
-console.log(a++); // outputs 10
-console.log(a); // outputs 11
+// console.log(numDeposits1000);
+
+// // Prefixed ++ operator
+// let a = 10;
+// console.log(++a); // outputs 11 if ++a, outputs 10 if a++
+// console.log(a); // outputs 11
+
+// // #3
+// const { deposits, withdrawals } = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     // the arrow function doesn't have an implicit return because it is using {} so we have to manually return it.
+//     (sums, cur) => {
+//       // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   ); // sums is the accumulator and it is the {deposits: 0, withdrawals: 0}
+
+// console.log(deposits, withdrawals);
+
+// // #4
+// // this is a nice title -> This Is a Nice Title
+// const convertTitleCase = function (title) {
+//   const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+//   const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+//   const titleCase = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+//     .join(' '); // word.slice(1) the rest of the string is input in starting at position number 1, since position zero was manipulated
+//   // exceptions.includes(word). If the array has the word then output the word, if it doesn't then manipulate
+//   return capitalize(titleCase);
+// };
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG title but not too long'));
+// console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+/*======================= Coding Challenge #4 =========================*/
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+GOOD LUCK 😀
+*/
+
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
+
+// // 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Formula: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+
+// dogs.forEach(dog => (dog.recFood = dog.weight ** 0.75 * 28)); // creates new property named recFood: the calculated number.
+// console.log(dogs);
+
+// // 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(dogSarah);
+// console.log(
+//   `Sarah's dog is eating too ${
+//     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+//   } `
+// );
+
+// // 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+// const ownersEatTooMuch = dogs
+//   .filter(dog => dog.curFood > dog.recFood)
+//   .flatMap(dog => dog.owners);
+// console.log(ownersEatTooMuch);
+
+// const ownersEatTooLittle = dogs
+//   .filter(dog => dog.curFood < dog.recFood)
+//   .flatMap(dog => dog.owners);
+// console.log(ownersEatTooLittle);
+
+// // 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+// // "Matilda and Alice and Bob's dogs eat too much!"
+// // "Sarah and John and Michael's dogs eat too little!"
+// console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+// console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// // 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+// console.log(`${dogs.some(dog => dog.curFood === dog.recFood)}`); // 'any' dog so that tells us we use the some() method
+
+// // 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+// // Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint)
+// // HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+// const checkEatingOkay = dog =>
+//   dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+// console.log(`${dogs.some(checkEatingOkay)}`);
+
+// // 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+// console.log(dogs.filter(checkEatingOkay));
+
+// // 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+// const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+// console.log(dogsSorted);
