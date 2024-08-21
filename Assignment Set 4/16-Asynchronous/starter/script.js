8439c0679a0aa630545a46f40ad3cdc616b8f5eb
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-08-21 12:51:26 -0600

    Working on 251 but there is an error
    Working on 251 but there is an error

--- Assignment Set 4/16-Asynchronous/starter/script.js
+++ Assignment Set 4/16-Asynchronous/starter/script.js
@@ -1,6 +1,42 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const renderCountry = function (data) {
+  const languages = Object.values(data.languages);
+  const currencies = Object.values(data.currencies);
 
-///////////////////////////////////////
+  const html = `
+  <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              data.population / 1000000
+            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>🗣️</span>${languages[0]}</p>
+            <p class="country__row"><span>💰</span>${currencies[0].name}</p>
+        </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (country) {
+  const btn = document.querySelector('.btn-country');
+  const countriesContainer = document.querySelector('.countries');
+
+  ///////////////////////////////////////
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+  });
+};
+
+getCountryAndNeighbor('portugal');
+// getCountryData('usa');
+// getCountryData('germany');
